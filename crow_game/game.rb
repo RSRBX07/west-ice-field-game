04467e4ef2fed52f54ee553874b0cbc3f8f665c4
monteil <john_monteil@hotmail.com>
2017-01-20 15:48:48 +0100

    add crow instance variable to game on init

--- crow_game/game.rb
+++ crow_game/game.rb
@@ -1,4 +1,6 @@
 require_relative './three.rb'
+require_relative '/crow.rb'
+
 class Game
   attr_reader :red_tree
   attr_reader :yellow_tree
@@ -10,6 +12,7 @@ class Game
     @yellow_tree = Three.new
     @green_tree = Three.new
     @blue_tree = Three.new
+    @crow = Crow.new
   end
 
 end
