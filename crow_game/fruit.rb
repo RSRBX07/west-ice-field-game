d335535bde126fe6ae95c8bf2132186baf766b83
DanaGhanem <ghanemdana@hotmail.com>
2017-01-20 14:47:01 +0100

    Update fruit.rb

--- crow_game/fruit.rb
+++ crow_game/fruit.rb
@@ -1,11 +1,9 @@
 class Fruit
   attr_reader :fruit
   
-  def create_fruit name, color,
-    name = ""
-    color = ""
-    number = 4 
-    @fruits = [name,color,number]
+  def create_fruit color
+    color = "" 
+    @fruits = [color]
   end
 
 end
