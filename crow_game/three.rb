5ef7fe201b3d594a46ed4477e307f223b0ef7ce0
didierclin <didier.c62@numericable.fr>
2017-01-20 12:05:32 +0100

    add private initialize method and initialize attributs fruit_three and fruit_number

--- crow_game/three.rb
+++ crow_game/three.rb
@@ -2,6 +2,14 @@ class Three
   attr_reader :fruit_three
   attr_reader :fruit_number
 
+  def loose_a_fruit
+    @fruit_number -= 1
+  end
 
+  private
+
+  def initialize
+    @fruit_number = 4
+  end
 
 end
